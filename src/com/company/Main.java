da19b22118a7b57f2dfba30bc95441ca263e48db
Vivekanand P V <vivekanandpv@outlook.com>
2020-04-22 18:53:17 +0530

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.BufferOverflowException;
+import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.channels.FileChannel;
@@ -9,6 +11,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -34,9 +38,18 @@ public class Main {
             readerBuffer.flip();
             IntBuffer intBuffer = readerBuffer.asIntBuffer();
 
-            for (int i = 0; i < intBuffer.limit(); i++) {
-                System.out.println("> " + intBuffer.get());
+            List<Integer> intsRead = new ArrayList<>();
+
+            try {
+                while(true) {
+                    intsRead.add(intBuffer.get());
+                }
+            } catch (BufferUnderflowException e) {
+                //...no action required here
             }
+
+            //using stream API
+            intsRead.forEach(System.out::println);
         }
     }
 }
