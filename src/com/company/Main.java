67226208ece2ddb31a53075f0608e4af106cf4a0
Vivekanand P V <vivekanandpv@outlook.com>
2020-04-22 21:20:29 +0530

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,27 +4,44 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        ExecutorService service = Executors.newCachedThreadPool();
+
         try (ServerSocket serverSocket = new ServerSocket(3000)) {
-            try(Socket incomingRequest = serverSocket.accept()) {
-                InputStream inputStream = incomingRequest.getInputStream();
-                OutputStream outputStream = incomingRequest.getOutputStream();
 
-                try (Scanner scanner = new Scanner(inputStream, "UTF-8")) {
-                    PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
+            while (true) {
+                service.submit(() -> {
+                    try {
+                        handleRequest(serverSocket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        }
+    }
 
-                    boolean done = false;
+    private static void handleRequest(ServerSocket serverSocket) throws IOException {
+        try(Socket incomingRequest = serverSocket.accept()) {
+            InputStream inputStream = incomingRequest.getInputStream();
+            OutputStream outputStream = incomingRequest.getOutputStream();
 
-                    while(!done && scanner.hasNextLine()) {
-                        String line = scanner.nextLine();
-                        printWriter.println(line);
+            try (Scanner scanner = new Scanner(inputStream, "UTF-8")) {
+                PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
 
-                        if (line.trim().equals("q")) {
-                            done = true;
-                        }
+                boolean done = false;
+
+                while(!done && scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    printWriter.println(line);
+
+                    if (line.trim().equals("q")) {
+                        done = true;
                     }
                 }
             }
