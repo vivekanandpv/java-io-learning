7f4070567c103719db7a25ed02914f46ad918896
Vivekanand P V <vivekanandpv@outlook.com>
2020-04-22 16:53:29 +0530

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,22 +2,41 @@ package com.company;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int b;
-        FileInputStream fileInputStream = new FileInputStream("readme.txt");
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 
-        try {
-            do {
-                b = fileInputStream.read();
-                if (b != -1) {
-                    System.out.print((char) b);
-                }
-            } while (b != -1);
-        } finally {
-            fileInputStream.close();
+        byteBuffer.putInt(100);
+        byteBuffer.putInt(200);
+        byteBuffer.putInt(300);
+        byteBuffer.putInt(400);
+
+        byteBuffer.flip();
+
+        try(FileChannel fileChannel = FileChannel.open(Paths.get("ints.bin"), StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+            fileChannel.write(byteBuffer);
+        }
+
+        System.out.println("File size: " + Files.size(Paths.get("ints.bin")) + " bytes");
+
+        try(FileChannel fileChannel = FileChannel.open(Paths.get("ints.bin"), StandardOpenOption.READ)) {
+            ByteBuffer readerBuffer = ByteBuffer.allocate(1024);
+            fileChannel.read(readerBuffer);
+            readerBuffer.flip();
+            IntBuffer intBuffer = readerBuffer.asIntBuffer();
+
+            for (int i = 0; i < intBuffer.limit(); i++) {
+                System.out.println("> " + intBuffer.get());
+            }
         }
     }
 }
