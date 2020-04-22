6ecfd896486f68a80f06bf3c935d96c0225a6314
Vivekanand P V <vivekanandpv@outlook.com>
2020-04-22 22:19:13 +0530

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,22 +2,42 @@ package com.company;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int b;
-        FileInputStream fileInputStream = new FileInputStream("readme.txt");
+        String siteName = "http://google.co.in";
+        URL url = new URL(siteName);
+        URLConnection urlConnection = url.openConnection();
 
-        try {
-            do {
-                b = fileInputStream.read();
-                if (b != -1) {
-                    System.out.print((char) b);
-                }
-            } while (b != -1);
-        } finally {
-            fileInputStream.close();
+        urlConnection.connect();
+
+        Map<String, List<String>> headers = urlConnection.getHeaderFields();
+
+        for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+            String key = entry.getKey();
+            for (String value : entry.getValue()) {
+                System.out.println(key + ": " + value);
+            }
+        }
+
+        System.out.println("------------------------------");
+
+        String encoding = urlConnection.getContentEncoding();
+
+        if (encoding == null) {
+            encoding = "UTF-8";
+        }
+
+        try(Scanner scanner = new Scanner(urlConnection.getInputStream(), encoding)) {
+            while (scanner.hasNextLine()) {
+                System.out.println("> " + scanner.nextLine());
+            }
         }
     }
 }
